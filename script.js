0a0dced361f243c662ab8505756e9fb5cfa9ecba
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-12 15:24:01 +0800

    add play round funcion

--- script.js
+++ script.js
@@ -1,65 +1,94 @@
-const gameOptions = ['rock', 'paper', 'scissors'];
+const gameOptions = ["rock", "paper", "scissors"];
+const prompt = require("prompt-sync")({ sigint: true });
+
 
 game();
 
 function game() {
   const gameRounds = 5;
+  let finishedRounds = 0;
+  let computerScore = 0;
+  let userScore = 0;
+  let roundResult;
 
-  //startTheGame(gameRounds);
-  //let playerSelection = getUserInput();
-  
-  //let userMessageAfterSelection = `Player Selected ${playerSelection}`;
-  //showTheMessage(userMessageAfterSelection);
-  
-  let computerSelection = getComputerChoice();
-  let computerMessageAfterSelection = `Computer Selected ${computerSelection}`;
-  showTheMessage(computerMessageAfterSelection);
+  startTheGame(gameRounds);
+  while (finishedRounds < gameRounds) {
+    let playerSelection = getUserInput();
+    let userMessageAfterSelection = `Player Selected: ${playerSelection}`;
+    showTheMessage(userMessageAfterSelection);
 
-  //for (let i = 0; i < gameRounds; ++i) {
-  //  const playerSelection = "rock";
-  //  const computerSelection = getComputerChoice();
-  //  playRound(playerSelection, computerSelection);
-  //}
+    let computerSelection = getComputerChoice();
+    let computerMessageAfterSelection = `Computer Selected: ${computerSelection}`;
+    showTheMessage(computerMessageAfterSelection);
+
+    roundResult = playRound(playerSelection, computerSelection);
+    
+    switch (roundResult) {
+      case "tie":
+        break;
+      case "user":
+          userScore++;
+          finishedRounds++;
+        break;
+      case "computer":
+        computerScore++;
+        finishedRounds++;
+        break;
+      default:
+        break;
+    } 
+  }
+  
+  
 }
 
-
 function getComputerChoice() {
-  
   let choice = Math.floor(Math.random() * gameOptions.length);
   return gameOptions[choice];
 }
 
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  
+let tieMessage = "It's a tie, let's replay the round";
 
-function getUserInput() {
-  const messageForUser =
-    "User it's your turn. Tell me your choice. Rock, paper or scissors?";
-
-  showTheMessage(messageForUser);
-  let userInput = readUserInput();
-  return userInput;
+  if (playerSelection === computerSelection) {
+    showTheMessage(tieMessage);
+    return 'tie';
+  }
+  
+  if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+  playerSelection === 'paper' && computerSelection === 'rock' ||
+  playerSelection === 'scissors' && computerSelection === 'paper') {
+    showTheMessage(`You win ${playerSelection} beats ${computerSelection}`);
+    return 'user';
+  } else {
+    showTheMessage(`You lose ${computerSelection} beats ${playerSelection}`);
+    return 'computer';
+  }
+    
 }
 
-function readUserInput() {
-  const readline = require("readline");
-
-  const r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  
+function getUserInput() {
   let answer;
-  
-  r1.question("Choice? ", function (answer) {
-    answer = answer;
-    r1.close();
-  });
-  return answer;
+
+  answer = prompt(
+    "User it's your turn. Tell me your choice. Rock, paper or scissors? > "
+  );
+
+  for (;;) {
+    if (gameOptions.includes(answer.toLowerCase())) {
+      return answer;
+    }
+
+    answer = prompt(
+      "Let's choose the right option. Rock, paper or scissors? > "
+    );
+  }
 }
 
 function showTheMessage(message) {
   console.log(message);
-  console.log("\n");
+  //console.log("\n");
 }
 
 function startTheGame(gameRounds) {
