1c0c11f069a93156b1505ce37709ffd07cdae5ec
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-18 18:18:56 +0800

    refactor script, user answer on a web

--- script.js
+++ script.js
@@ -1,63 +1,54 @@
 const GAME_OPTIONS = ["rock", "paper", "scissors"];
+
 const RESULTS = {
   TIE: "tie",
   USER: "user",
   COMPUTER: "computer",
 };
 
-const prompt = require("prompt-sync")({ sigint: true });
+const RESULT_BOX = document.querySelector(".results");
 
 game();
 
 function game() {
-  const gameRounds = 5;
-  let finishedRounds = 0;
-  let computerScore = 0;
-  let userScore = 0;
-  let roundResult;
+  const options = document.querySelectorAll("button.option");
+  console.log(options);
+  options.forEach(playRound);
+}
 
-  startTheGame(gameRounds);
-  while (finishedRounds < gameRounds) {
-    let playerSelection = getUserInput();
-    let userMessageAfterSelection = `Player Selected: ${playerSelection}`;
-    showTheMessage(userMessageAfterSelection);
+function playRound(option) {
+  let playerSelection;
 
+  option.addEventListener("click", (event) => {
+    playerSelection = event.currentTarget.className.split(" ")[0];
+    console.log(event.currentTarget);
     let computerSelection = getComputerChoice();
-    let computerMessageAfterSelection = `Computer Selected: ${computerSelection}`;
-    showTheMessage(computerMessageAfterSelection);
-
-    roundResult = playRound(playerSelection, computerSelection);
-
-    switch (roundResult) {
-      case RESULTS.TIE:
-        break;
-      case RESULTS.USER:
-        userScore++;
-        finishedRounds++;
-        break;
-      case RESULTS.COMPUTER:
-        computerScore++;
-        finishedRounds++;
-        break;
-      default:
-        break;
-    }
-
-    if (userScore === 3 || computerScore === 3) {
-      break;
-    }
-
-    showTheMessage(
-      `User Score is: ${userScore}, Computer score is: ${computerScore}
-       Played rounds: ${finishedRounds}`
+    
+    console.log(
+      `Player option: ${playerSelection}, Computer option: ${computerSelection}`
     );
-  }
+    if (playerSelection === computerSelection) {
+      showTheMessage("It's a tie, let's replay the round");
+      return RESULTS.TIE;
+    }
 
-  if (userScore > computerScore) {
-    showTheMessage("You are the winner of our competition!");
-  } else {
-    showTheMessage("Computer is the winner of our competition!");
-  }
+    if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      showTheMessage(`You win! ${playerSelection} beats ${computerSelection}`);
+      return RESULTS.USER;
+    } else {
+      showTheMessage(`You lose! ${computerSelection} beats ${playerSelection}`);
+      return RESULTS.COMPUTER;
+    }
+  });
+}
+
+function showTheMessage(message) {
+  console.log(message);
+  RESULT_BOX.textContent = message;
 }
 
 function getComputerChoice() {
@@ -65,48 +56,53 @@ function getComputerChoice() {
   return GAME_OPTIONS[choice];
 }
 
-function playRound(playerSelection, computerSelection) {
+//function game() {
+//  const gameRounds = 5;
+//  let finishedRounds = 0;
+//  let computerScore = 0;
+//  let userScore = 0;
+//  let roundResult;
 
-  if (playerSelection === computerSelection) {
-    showTheMessage("It's a tie, let's replay the round");
-    return RESULTS.TIE;
-  }
+//  startTheGame(gameRounds);
+//  while (finishedRounds < gameRounds) {
+//    let playerSelection = getUserInput();
+//    let userMessageAfterSelection = `Player Selected: ${playerSelection}`;
+//    showTheMessage(userMessageAfterSelection);
 
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    showTheMessage(`You win! ${playerSelection} beats ${computerSelection}`);
-    return RESULTS.USER;
-  } else {
-    showTheMessage(`You lose! ${computerSelection} beats ${playerSelection}`);
-    return RESULTS.COMPUTER;
-  }
-}
+//    let computerSelection = getComputerChoice();
+//    let computerMessageAfterSelection = `Computer Selected: ${computerSelection}`;
+//    showTheMessage(computerMessageAfterSelection);
 
-function getUserInput() {
-  let answer;
+//    roundResult = playRound(playerSelection, computerSelection);
 
-  answer = prompt(
-    "User it's your turn. Tell me your choice. Rock, paper or scissors? > "
-  );
+//    switch (roundResult) {
+//      case RESULTS.TIE:
+//        break;
+//      case RESULTS.USER:
+//        userScore++;
+//        finishedRounds++;
+//        break;
+//      case RESULTS.COMPUTER:
+//        computerScore++;
+//        finishedRounds++;
+//        break;
+//      default:
+//        break;
+//    }
 
-  while(!GAME_OPTIONS.includes(answer.toLowerCase())) {
-    answer = prompt(
-      "Let's choose the right option. Rock, paper or scissors? > "
-    );
-    }
-    return answer.toLowerCase();
-}
+//    if (userScore === 3 || computerScore === 3) {
+//      break;
+//    }
 
-function showTheMessage(message) {
-  console.log(message);
-}
+//    showTheMessage(
+//      `User Score is: ${userScore}, Computer score is: ${computerScore}
+//       Played rounds: ${finishedRounds}`
+//    );
+//  }
 
-function startTheGame(gameRounds) {
-  const introductionWords = `We are starting our Rock-Paper-Scissors game tour.There will be ${gameRounds} game rounds! 
-  Tour against computer is OOOOpend!!!!`;
-
-  showTheMessage(introductionWords);
-}
+//  if (userScore > computerScore) {
+//    showTheMessage("You are the winner of our competition!");
+//  } else {
+//    showTheMessage("Computer is the winner of our competition!");
+//  }
+//}
